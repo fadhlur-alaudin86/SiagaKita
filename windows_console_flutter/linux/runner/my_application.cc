b0732a9a37a98c85ea59d57279839802c9fea332
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2026-05-05 07:33:45 +0700

    chore: rename application to SiagaKita and update platform-specific icons and configurations

--- windows_console_flutter/linux/runner/my_application.cc
+++ windows_console_flutter/linux/runner/my_application.cc
@@ -45,14 +45,25 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "windows_console_flutter");
+    gtk_header_bar_set_title(header_bar, "SiagaKita");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "windows_console_flutter");
+    gtk_window_set_title(window, "SiagaKita");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
+  
+  g_autofree gchar* exe_path = g_file_read_link("/proc/self/exe", nullptr);
+  if (exe_path) {
+    g_autofree gchar* exe_dir = g_path_get_dirname(exe_path);
+    g_autofree gchar* icon_path = g_build_filename(exe_dir, "data", "flutter_assets", "assets", "app_icon.png", nullptr);
+    g_autoptr(GError) error = nullptr;
+    g_autoptr(GdkPixbuf) icon = gdk_pixbuf_new_from_file(icon_path, &error);
+    if (icon) {
+      gtk_window_set_icon(window, icon);
+    }
+  }
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(
