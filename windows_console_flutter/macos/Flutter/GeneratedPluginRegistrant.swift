1110ec988ad97f6e2bde4ea968397403837116d0
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2026-05-07 01:19:22 +0700

    chore: upgrade connectivity_plus, flutter_map, fl_chart, and flutter_secure_storage dependencies

--- windows_console_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ windows_console_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
