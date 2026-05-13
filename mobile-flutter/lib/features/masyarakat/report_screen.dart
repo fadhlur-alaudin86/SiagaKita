1e906d73d20292a04589b932d38a73d5364a68be
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2026-05-13 09:49:59 +0700

    refactor: remove unused image_picker import from report_screen.dart

--- mobile-flutter/lib/features/masyarakat/report_screen.dart
+++ mobile-flutter/lib/features/masyarakat/report_screen.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:record/record.dart';
