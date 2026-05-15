52777233727e4618a0aa25595f6a0f3d13162d92
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2026-05-15 07:05:38 +0700

    chore: rename project to SiagaKita Console and update dependencies

--- windows_console_flutter/test/widget_test.dart
+++ windows_console_flutter/test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:windows_console_flutter/app.dart';
+import 'package:siagakita_console/app.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
